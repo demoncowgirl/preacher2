45d61977128a53bd97fca86ba5ffa1c44438be39
Beth Salvatore <bethsalvatore2018@gmail.com>
2018-05-29 08:52:04 -0400

    final commit

--- gulpfile.js
+++ gulpfile.js
@@ -4,12 +4,6 @@ var sass = require('gulp-sass');
 var runSequence = require('run-sequence');
 
 
-// function to copy files from app dir to dist dir
-//gulp.task('copy', function() {
-  //log("Copying all html files from app directory to dist directory");
-	//return gulp.src('app/**/*.html')
-		///pipe(gulp.dest('dist/'))
-//});
 
 // task to copy all htm/html files from app directory to dist directory
 gulp.task('copyhtml', function() {
@@ -17,25 +11,17 @@ gulp.task('copyhtml', function() {
 	.pipe(gulp.dest('dist/'))
  });
 
-// task to copy all css files from app directory to dist directory
-gulp.task('copycss', function() {
-	return gulp.src('app/*.css')
-		.pipe(gulp.dest('dist/css/'))
-});
+gulp.task('copyimages', function(){
+  return gulp.src('app/images/*.+(png|jpg)')
+  .pipe(gulp.dest('dist/images'))
+})
 
 // function to change sass code to css
 gulp.task('sass', function() {
-	return gulp.src('app/*.scss')
+	return gulp.src('app/scss/*.scss')
 		.pipe(sass())
 		.pipe(gulp.dest('dist/css/'))
 });
-
-// function to trigger the copy function when new html files are
-// added to the app file
-//gulp.task('watch', function() {
-  //log("Watching html files for modifications");
-	//gulp.watch('app/**/*', ['copyhtml', 'copycss' 'sass'])
-//});
 
 
 gulp.task('default', function(callback) {
